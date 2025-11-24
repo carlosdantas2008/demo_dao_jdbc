a45332275c69f28da54eb574f472697af7c4c89e
Carlos Pereira <carlospereira@carloss-mbp.lan>
2025-11-23 19:13:11 -0800

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,11 +32,16 @@ public class Program {
 		}
 		
 		System.out.println("==== TEST 4: seller insert ====");
-		
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department); 
-				
 		sellerDao.inser(newSeller);
-		
 		System.out.println("Inserted! New Id: " + newSeller.getId());
+		
+		System.out.println("==== TEST 5: seller update ====");
+		Seller updatedSeller = new Seller();
+		updatedSeller = sellerDao.findById(1);
+		updatedSeller.setName("Martha Waine");
+		sellerDao.update(updatedSeller);
+		System.out.println("Update completed!");
+		
 	}
 }
